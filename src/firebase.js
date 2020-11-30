7a601a0f688fdca12118b0fb831bef832c5eebee
educalo77 <educalo@outlook.com>
2020-11-30 11:32:10 -0300

    new project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlesh9mR6Oa3z6tLfFcg2HjPAqSIGTo8o",
+  authDomain: "edutube-fe6b5.firebaseapp.com",
+  databaseURL: "https://edutube-fe6b5.firebaseio.com",
+  projectId: "edutube-fe6b5",
+  storageBucket: "edutube-fe6b5.appspot.com",
+  messagingSenderId: "1044893306668",
+  appId: "1:1044893306668:web:f731687fd2189537322689",
+  measurementId: "G-EL8CVZKWXX"
+};
