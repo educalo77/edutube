ec78701edece9774801b32d6df0782a222b06a07
Eduardo Calo <66800193+educalo77@users.noreply.github.com>
2020-12-01 18:53:10 -0300

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyB5TSLM8fSFv5GH0bscaTl6Tv9d9Oms_y4';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
